a32efa881727471d3c350c15e52cd5dcda322d67
Mauricio <mauricio.pautasso@gmail.com>
2012-12-06 06:35:49 +1100

    Create initial app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyCordobaWebSite::Application.config.secret_token = 'f9e3c29a7536d444c18116c45fe1a3e475ceee961816ffa8f1506b0d0cb701b73ecfe6985a7a1bb15b677e7f0803877b8dc128eab3facc1cb458ae9e4f47b8fa'
