92a24b4fca9bc39743272590928c472def759c90
Diego Mónaco <dfmonaco@gmail.com>
2012-12-08 10:57:37 -0300

    changed gitnesse repository_url to use git protocol instead of https to avoid writing username and pasword each time

--- config/gitnesse.rb
+++ config/gitnesse.rb
@@ -1,5 +1,5 @@
 Gitnesse.configure do |config|
-  config.repository_url   = "https://github.com/ruby-cordoba/ruby-cordoba-web-site.wiki.git"
+  config.repository_url   = "git@github.com:ruby-cordoba/ruby-cordoba-web-site.wiki.git"
   config.annotate_results = true
   config.info             = "Cucumber wiki"
 end
